50b245f566d9f9589c62a6acf411e4dd6086ef7d
Francois Duguay <francois.duguay@epita.fr>
2024-04-27 18:07:01 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -1,46 +1,81 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
 
 namespace Project
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var test = new InsuranceServiceTests();
+            test.RuralAgeLessThan18_ReturnsZero();
+            test.RuralAgeBetween18And30_ReturnsCorrect();
+            test.RuralAgeGreaterThan30_ReturnsCorrect();
+            test.UrbanAgeLessThan18_ReturnsZero();
+            test.UrbanAgeBetween18And30_ReturnsCorrect();
+            test.UrbanAgeGreaterThan30_ReturnsCorrect();
+            test.UnknownLocation_ReturnsZero();
+            test.RuralAgeGreaterThan50_ReturnsCorrect();
+            test.UrbanAgeGreaterThan50_ReturnsCorrect();
+            return 0;
         }
     }
 
-    class InsuranceService
+    public class InsuranceService
     {
-        public double CalcPremium(int age, string location)
-            {
-                double premium;
+        private readonly DiscountService discountService;
 
-                if (location == "rural")
-	        if ((age >= 18) && (age < 30))
+        public InsuranceService(DiscountService discountService)
+        {
+            this.discountService = discountService;
+        }
+
+        public double CalcPremium(int age, string location)
+        {
+            double premium;
+
+            if (location == "rural")
+            {
+                if (age >= 18 && age < 30)
                     premium = 5.0;
-                else
-                if (age >= 31)
+                else if (age >= 31)
                     premium = 2.50;
                 else
                     premium = 0.0;
-        else
-            if (location == "urban")
-	            if ((age >= 18) && (age <= 35))
+            }
+            else if (location == "urban")
+            {
+                if (age >= 18 && age <= 35)
                     premium = 6.0;
-                else
-                   if (age >= 36)
+                else if (age >= 36)
                     premium = 5.0;
                 else
                     premium = 0.0;
-        else
-                    premium = 0.0;
-
-                DiscountService ds = new DiscountService();
-                double discount = ds.GetDiscount();
-                if (age >= 50)
-                    premium = premium * discount;
-                return premium;
             }
+            else
+            {
+                premium = 0.0;
+            }
+
+            double discount = discountService.GetDiscount();
+            if (age >= 50)
+                premium *= discount;
+
+            return premium;
+        }
+    }
+
+    public class DiscountService()
+    {
+        public virtual double GetDiscount()
+        {
+            return 0.8;
         }
 }
+    }
---
+++ ProgramTest.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using Moq;
+using Project;
+
+[TestFixture]
+public class InsuranceServiceTests
+{
+    [Test]
+    public void RuralAgeLessThan18_ReturnsZero()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(17, "rural");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(0.00));
+    }
+
+    [Test]
+    public void RuralAgeBetween18And30_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(25, "rural");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(5.00));
+    }
+
+    [Test]
+    public void RuralAgeGreaterThan30_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(35, "rural");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(2.50));
+    }
+
+    [Test]
+    public void UrbanAgeLessThan18_ReturnsZero()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(17, "urban");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(0.0));
+    }
+
+    [Test]
+    public void UrbanAgeBetween18And30_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(19, "urban");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(6.0));
+    }
+
+    [Test]
+    public void UrbanAgeGreaterThan30_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(42, "urban");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(5.0));
+    }
+
+    [Test]
+    public void UnknownLocation_ReturnsZero()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(24, "Moon");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(0.0));
+    }
+
+    [Test]
+    public void RuralAgeGreaterThan50_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(69, "rural");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(2.5));
+    }
+
+    [Test]
+    public void UrbanAgeGreaterThan50_ReturnsCorrect()
+    {
+        // Arrange
+        var mockDiscountService = new Mock<DiscountService>();
+        mockDiscountService.Setup(ds => ds.GetDiscount()).Returns(1.0); // Assuming no discount
+
+        var insuranceService = new InsuranceService(mockDiscountService.Object);
+
+        // Act
+        var premium = insuranceService.CalcPremium(47, "urban");
+
+        // Assert
+        Assert.That(premium, Is.EqualTo(5.0));
+    }
+
+}
